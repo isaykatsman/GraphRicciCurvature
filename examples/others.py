4123bfa08b4f787e988de677e8282b18c2496b9e
Calin Cruceru <calin@rosedu.org>
2019-06-21 13:51:00 +0200

    More experiments; toy sphere, etc

--- examples/others.py
+++ examples/others.py
@@ -23,7 +23,7 @@ def get_edge_curvatures(graph):
 
 
 def plot_curvatures(curvatures, name):
-    plt.hist(curvatures, bins=20)
+    plt.hist(curvatures)
     plt.xlabel('Curvature')
     plt.ylabel('Edges')
     plt.savefig('{}_curvatures.png'.format(name))
@@ -62,7 +62,7 @@ def tree():
 
 
 def plot_sphere(X, name):
-    fig = plt.figure()
+    fig = plt.figure(figsize=(15, 15))
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(X[:, 0], X[:, 1], X[:, 2])
     plt.savefig('{}.png'.format(name))
@@ -108,29 +108,77 @@ def sphere():
     plot_curvatures(o_curvatures, 'sphere_ollivier')
 
 
-def regular_sphere():
-    # Algorithm from https://www.cmu.edu/biolphys/deserno/pdf/sphere_equi.pdf
-    n_samples = 1000
-    a = 4 * np.pi / n_samples
-    d = np.sqrt(a)
-    m_nu = int(np.pi / d)
-    d_nu = np.pi / m_nu
-    d_phi = a / d_nu
+def search_smallest_dist_for_connected_graph(dists):
+    # search for the smallest distance which gives a single connected component
+    low = 0.01
+    high = 10.0
+    while high - low > 1e-5:
+        mid = (low + high) / 2
+        graph = nx.Graph()
+        graph.add_edges_from(np.argwhere(dists < mid))
+        graph.remove_edges_from(graph.selfloop_edges())
 
-    X = []
-    for m in range(m_nu):
-        nu = np.pi * (m + 0.5) / m_nu
-        m_phi = int(2 * np.pi * np.sin(nu) / d_phi)
-        for n in range(m_phi):
-            phi = 2 * np.pi * n / m_phi
-            x = np.array([
-                    np.sin(nu) * np.cos(phi),
-                    np.sin(nu) * np.sin(phi),
-                    np.cos(nu)
-            ])
-            X.append(x)
-    n_samples = len(X)
-    print('Number of points: ', n_samples)
+        if nx.number_connected_components(graph) > 1:
+            low = mid
+        else:
+            high = mid
+
+    print('Neighbor threshold distance: ', high)
+    graph = nx.Graph()
+    graph.add_edges_from(np.argwhere(dists < high))
+    graph.remove_edges_from(graph.selfloop_edges())
+    assert nx.number_connected_components(graph) == 1
+
+    return graph
+
+
+def largest_smallest_connecting_distance(dists):
+    dists_copy = dists.copy()
+    np.fill_diagonal(dists_copy, np.inf)
+    min_dists = np.min(dists_copy, axis=1)
+    neigh_threshold = np.max(min_dists)
+
+    graph = nx.Graph()
+    # multiply by some threshold to make sure it is connected; note that the
+    # above formula doesn't guarantees connectedness
+    graph.add_edges_from(np.argwhere(dists < 1.3 * neigh_threshold))
+    graph.remove_edges_from(graph.selfloop_edges())
+    assert nx.number_connected_components(graph) == 1
+
+    return graph
+
+
+def regular_sphere(toy=False):
+    if toy:
+        # yapf: disable
+        X = [
+            [0, 0, 1], # north pole
+            [1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], # equator
+            [0, 0, -1], # south pole
+        ]
+        # yapf: enable
+    else:
+        # Algorithm from https://www.cmu.edu/biolphys/deserno/pdf/sphere_equi.pdf
+        n_samples = 100
+        a = 4 * np.pi / n_samples
+        d = np.sqrt(a)
+        m_nu = int(np.pi / d)
+        d_nu = np.pi / m_nu
+        d_phi = a / d_nu
+
+        X = []
+        for m in range(m_nu):
+            nu = np.pi * (m + 0.5) / m_nu
+            m_phi = int(2 * np.pi * np.sin(nu) / d_phi)
+            for n in range(m_phi):
+                phi = 2 * np.pi * n / m_phi
+                x = np.array([
+                        np.sin(nu) * np.cos(phi),
+                        np.sin(nu) * np.sin(phi),
+                        np.cos(nu)
+                ])
+                X.append(x)
+    print('Number of points: ', len(X))
 
     # plot it to make sure it looks 'regular'
     X = np.array(X)
@@ -142,19 +190,9 @@ def regular_sphere():
     print('Finished computing distances')
 
     # search for the smallest distance which gives a single connected component
-    low = 0.1
-    high = 1.0
-    while high - low > 1e-5:
-        mid = (low + high) / 2
-        graph = nx.Graph()
-        graph.add_edges_from(np.argwhere(dists < mid))
-        graph.remove_edges_from(graph.selfloop_edges())
-
-        if nx.number_connected_components(graph) > 1:
-            low = mid
-        else:
-            high = mid
-    assert nx.number_connected_components(graph) == 1
+    #   graph = search_smallest_dist_for_connected_graph(dists)
+    # even better: use the largest-smallest connecting distance
+    graph = largest_smallest_connecting_distance(dists)
 
     # degree distribution; should be small
     plot_degree_distribution(graph, 'regular_sphere')
@@ -163,8 +201,11 @@ def regular_sphere():
     graph = ricciCurvature(graph, alpha=0.5, method='OTD')
     graph = formanCurvature(graph)
     o_curvatures, f_curvatures = get_edge_curvatures(graph)
-    plot_curvatures(o_curvatures, 'regular_sphere_ollivier')
-    plot_curvatures(f_curvatures, 'regular_sphere_forman')
+    if toy:
+        print(o_curvatures, f_curvatures)
+    else:
+        plot_curvatures(o_curvatures, 'regular_sphere_ollivier')
+        plot_curvatures(f_curvatures, 'regular_sphere_forman')
 
 
 def cycle():
@@ -212,6 +253,33 @@ def erdos_renyi():
     plot_curvatures(f_curvs, 'f_gnp')
 
 
+def hypercube():
+    g = nx.hypercube_graph(5)
+    g = ricciCurvature(g, alpha=0.5, method='OTD')
+    g = formanCurvature(g)
+    o_curvs, f_curvs = get_edge_curvatures(g)
+    print(o_curvs, f_curvs)
+
+
+def grid():
+    g = nx.grid_graph([5, 5, 5], periodic=True)
+    g = ricciCurvature(g, alpha=0.5, method='OTD')
+    g = formanCurvature(g)
+    o_curvs, f_curvs = get_edge_curvatures(g)
+    plot_curvatures(o_curvs, 'o_grid')
+    plot_curvatures(f_curvs, 'f_grid')
+
+
+def small_sphere():
+    g = nx.Graph()
+    g.add_edges_from([(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 5),
+                      (2, 4), (2, 5), (3, 4), (3, 5), (4, 5)])
+    g = ricciCurvature(g, alpha=0.5, method='OTD')
+    g = formanCurvature(g)
+    o_curvs, f_curvs = get_edge_curvatures(g)
+    print(o_curvs, f_curvs)
+
+
 def main():
     # full_graph()
     # tree()
@@ -219,7 +287,11 @@ def main():
     # regular_sphere()
     # cycle()
     # balanced_tree()
-    erdos_renyi()
+    # erdos_renyi()
+    # hypercube()
+    # grid()
+    # small_sphere()
+    regular_sphere(toy=True) # should be the same as `small_sphere()`
 
 
 if __name__ == '__main__':
